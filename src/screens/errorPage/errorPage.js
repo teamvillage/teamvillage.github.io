ec50ef3febd6644f346473720ba4e2f4446f3cd4
MS-Harine <Minseok.H.Song@gmail.com>
2023-10-16 16:18:44 +0900

    Add redirect to 404 page

--- src/screens/errorPage/errorPage.js
+++ src/screens/errorPage/errorPage.js
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import styles from './errorPage.module.scss';
 
 export default function ErrorPage() {
@@ -12,6 +12,9 @@ export default function ErrorPage() {
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <Link to={'/login'}>
+        <p>Click Here to Start</p>
+      </Link>
     </div>
   );
 }
